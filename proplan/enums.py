625ef152d8fd709dad06d74df179dec3dbb3d849
AnaTomljenovic <akikas@unipu.hr>
2025-09-09 20:58:41 +0200

    add missing day off enum

--- proplan/enums.py
+++ proplan/enums.py
@@ -1,24 +1,25 @@
 from enum import Enum
 
-
 class Availability(str, Enum):
     FREE = "Free"
     BUSY = "Busy"
-
 
 class Role(str, Enum):
     ADMIN = "Admin"
     MANAGER = "Manager"
     WORKER = "Worker"
 
-
 class ProjectStatus(str, Enum):
     STARTED = "Started"
     ONGOING = "Ongoing"
     FINISHED = "Finished"
 
-
 class TaskStatus(str, Enum):
     OPEN = "Open"
     IN_PROGRESS = "In Progress"
     DONE = "Done"
+
+class DayOffType(str, Enum):
+    HOLIDAY = "Holiday"
+    SICK_LEAVE = "Sick Leave"
+    DAY_OFF = "Day Off"
